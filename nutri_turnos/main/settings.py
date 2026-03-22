905b74c679ddccc1a24259464213064648caa489
GuilleTorres <guilletorres81@gmail.com>
2026-03-22 02:33:56 -0300

    Settings staticos

--- nutri_turnos/main/settings.py
+++ nutri_turnos/main/settings.py
@@ -80,7 +80,7 @@ USE_TZ = True
 
 # 📦 Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = '/root/nutri-turnos/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
