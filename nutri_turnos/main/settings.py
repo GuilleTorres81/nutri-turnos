4c3d4d1360371baf29a136bf78f7198eeb686ad6
GuilleTorres <guilletorres81@gmail.com>
2026-03-22 02:48:19 -0300

    Cambios de hosts permitidos

--- nutri_turnos/main/settings.py
+++ nutri_turnos/main/settings.py
@@ -8,7 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = [
+    'nutriturnos.duckdns.org',
+    '34.121.166.228',
+]
 
 # Apps
 INSTALLED_APPS = [
